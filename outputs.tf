c4560b8c167cddc511f7cc3755a57f5d9b54bd4a
Bernard Maltais <bmaltais@gmail.com>
2020-06-29 15:06:53 +0000

    Fix output

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@ output "recovery_services_vault" {
   depends_on = [azurerm_recovery_services_vault.recovery_services_vault]
 
   description = "Returns the full set of recovery_services_vault created"
-  value = azurerm_recovery_services_vault.recovery_services_vault[0]
+  value = azurerm_recovery_services_vault.recovery_services_vault
 }
